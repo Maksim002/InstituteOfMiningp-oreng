66a3fa755fad8a1cfec443f60f8c1b237fbecd6c
Maksim002 <inoplanee@mail.ru>
2020-05-26 17:01:23 +0600

    Initial commit

---
+++ app/src/main/java/com/example/instituteofmining/main/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.instituteofmining.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.instituteofmining.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_staff,
+                R.id.navigation_life,
+                R.id.navigation_directions
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}
---
+++ app/src/main/java/com/example/instituteofmining/splash/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.instituteofmining.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.instituteofmining.R
+import com.example.instituteofmining.main.MainActivity
+
+class SplashActivity : AppCompatActivity(){
+    private val SPLASH_TIME: Long = 1500
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        initSplashScreen()
+    }
+
+    private fun initSplashScreen() {
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME)
+
+    }
+}
