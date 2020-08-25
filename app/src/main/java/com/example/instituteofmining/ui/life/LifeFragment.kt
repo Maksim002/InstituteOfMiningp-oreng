9ab54b584531742fb22ac5f2ef5c7c39eb388539
Maksim002 <inoplanee@Mail.ru>
2020-08-25 21:17:09 +0600

    new

--- app/src/main/java/com/example/instituteofmining/ui/life/LifeFragment.kt
+++ app/src/main/java/com/example/instituteofmining/ui/life/LifeFragment.kt
@@ -1,15 +1,13 @@
 package com.example.instituteofmining.ui.life
 
-import android.content.Intent
-import android.net.Uri
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.bumptech.glide.Glide
 import com.example.instituteofmining.R
-import kotlinx.android.synthetic.main.fragment_life.*
+
 
 class LifeFragment : Fragment() {
 
